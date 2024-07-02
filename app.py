0f5715a07ba924bfcaa8bfde72eea66ecd980ff1
Muhammad-Shah <msh87376@gmail.com>
2024-07-02 15:03:23 +0500

    app updated

--- app.py
+++ app.py
@@ -12,11 +12,25 @@ model = BertForSequenceClassification.from_pretrained(load_directory)
 model.to(device)
 
 # Define Streamlit app
+
+
 def main():
+    """
+    Runs the Sentiment Analysis App.
+
+    This function displays a Streamlit app that allows users to input a sentence and analyze its sentiment. The app prompts the user to enter a sentence using a text input field. When the user clicks the "Analyze" button, the function tokenizes the input text, passes it through a pre-trained BERT model, and predicts the sentiment of the sentence. The predicted sentiment and the confidence score are then displayed on the app.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     st.title('Sentiment Analysis App')
 
     # User input
-    text_input = st.text_input('Enter a sentence:', placeholder='Movies was fantastic!')
+    text_input = st.text_input(
+        'Enter a sentence:', placeholder='Movies was fantastic!')
 
  # Analyze sentiment
     if st.button('Analyze'):
@@ -28,7 +42,9 @@ def main():
             pred_prob = torch.sigmoid(logits)
             pred = torch.argmax(pred_prob, dim=1)
             sentiment = LABELS[pred.item()]
-            st.write(f'The sentiment of the sentence is: {sentiment} with a probability of {pred_prob.max().item() * 100:.2f}% confidence.')
+            st.write(
+                f'The review is: {sentiment} with a probability of {pred_prob.max().item() * 100:.2f}% confidence.')
+
 
 if __name__ == '__main__':
     main()
